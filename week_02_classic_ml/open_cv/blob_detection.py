dbcc6dcc28b3dd468b1167abcd09426171fc38c1
duongductrung1708 <trungyna1708@gmail.com>
2026-05-28 17:08:54 +0700

    .

--- week_02_classic_ml/open_cv/blob_detection.py
+++ week_02_classic_ml/open_cv/blob_detection.py
@@ -1,21 +1,26 @@
-# Standard imports
 import cv2
-import numpy as np;
 
-# Read image
-im = cv2.imread("./resource/cat.jpg", cv2.IMREAD_GRAYSCALE)
+# read the image
+image = cv2.imread('./resource/cat.jpg')
 
-# Set up the detector with default parameters.
-detector = cv2.SimpleBlobDetector()
+# convert the image to grayscale format
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Detect blobs.
-keypoints = detector.detect(im)
+# apply binary thresholding
+ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+# visualize the binary image
+cv2.imshow('Binary image', thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-# Show keypoints
-cv2.imshow("Keypoints", im_with_keypoints)
+# detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+                                     
+# draw contours on the original image
+image_copy = image.copy()
+cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+               
+# see the results
+cv2.imshow('None approximation', image_copy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
